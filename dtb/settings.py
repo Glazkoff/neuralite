9d56fec0fa346365f311f828eeeb592c29e7bf79
Nikita Glazkov <zitrnik@gmail.com>
2023-08-03 11:53:26 +0300

    DEV: test change

--- dtb/settings.py
+++ dtb/settings.py
@@ -121,11 +121,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = "ru-RU"
-TIME_ZONE = "UTC+3"
+TIME_ZONE = "Europe/Moscow"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
