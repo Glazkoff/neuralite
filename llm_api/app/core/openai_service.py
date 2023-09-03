c35269ff41a7a92bd1083110b84ba0cf4d3a93aa
Nikita Glazkov <zitrnik@gmail.com>
2023-09-03 13:54:25 +0300

    DEV: update prompts

--- llm_api/app/core/openai_service.py
+++ llm_api/app/core/openai_service.py
@@ -10,27 +10,27 @@ class OpenAIService:
     def generate_summary(self, text: str) -> str:
         messages = [
             {
-                "content": "Could you please provide a summary of the given text, including all key points and supporting details? The summary should be comprehensive and accurately reflect the main message and arguments presented in the original text, while also being concise and easy to understand. To ensure accuracy, please read the text carefully and pay attention to any nuances or complexities in the language. Additionally, the summary should avoid any personal biases or interpretations and remain objective and factual throughout. Use only neutral language. Write only in Russian. Text:",
+                "content": f"Could you please provide a summary of the given text, including all key points and supporting details? The summary should be comprehensive and accurately reflect the main message and arguments presented in the original text, while also being concise and easy to understand. To ensure accuracy, please read the text carefully and pay attention to any nuances or complexities in the language. Ensure that the meaning of the original text is not changed. Additionally, the summary should avoid any personal biases or interpretations and remain objective and factual throughout. If the text does not contain any information, then simply write \"Нет информации.\". Use only neutral language. Write only in Russian. Please execute the command for the following text: '{text}'",
                 "role": "system",
             },
-            {"role": "user", "content": text},
         ]
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages,
+            temperature=0.1,
         )
         return response.choices[0].message.get("content", "")
 
     def extract_facts(self, text: str) -> str:
         messages = [
             {
-                "content": "Your task is to extract facts from the text I give you in up to 10 bulletpoints. Pick a corresponding number emoji (like 1, 2, 3) for each bullet point. To ensure accuracy, please read the text carefully and pay attention to any nuances or complexities in the language. Additionally, the summary should avoid any personal biases or interpretations and remain objective and factual throughout. Sort list by importance of fact. Use only neutral language. Reply in Russian.",
+                "content": f"Your task is to extract facts from the text I give you in up to 10 bulletpoints. Pick a corresponding number emoji (like 1, 2, 3) for each bullet point. To ensure accuracy, please read the text carefully and pay attention to any nuances or complexities in the language. Additionally, the summary should avoid any personal biases or interpretations and remain objective and factual throughout. Sort list by importance of fact. Use only neutral language. If the text does not contain any information, then simply write \"Нет информации.\". Reply in Russian. Please execute the command for the following text: '{text}'",
                 "role": "system",
             },
-            {"role": "user", "content": text},
         ]
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages,
+            temperature=0.1,
         )
         return response.choices[0].message.get("content", "")
