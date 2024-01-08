668650a6d28329e4849807c27e03b5e7aacdf1b4
Nikita Glazkov <zitrnik@gmail.com>
2024-01-08 23:53:15 +0300

    DEV: add base for inference LORA adapter

--- langchain_summ/app/chains/chain.py
+++ langchain_summ/app/chains/chain.py
@@ -1,35 +1,38 @@
 from langchain.schema.output_parser import StrOutputParser
-from transformers import AutoTokenizer
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain.prompts import PromptTemplate
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    pipeline,
+)
+import torch
 
-
-# Create chain
-# model = "ai-forever/rugpt3small_based_on_gpt2"
-model = "AlexWortega/instruct_rugptSmall"
-tokenizer = AutoTokenizer.from_pretrained(model)
-rugpt = HuggingFacePipeline.from_model_id(
-    # The name of the model on the HuggingFace server (for example "gpt2" by default) or the path to the model on disk
-    # model_id="gpt2",
-    model_id=model,
-    # Other options are possible: "text2text-generation", "text-generation" (default), "summarization"
-    task="text-generation",
-    # task="text2text-generation",
-    # Settings available for the model
-    device=-1,
-    # pipeline_kwargs={"max_new_tokens": 10},
-    pipeline_kwargs={
-        "temperature": 0.01,
-        "max_length": 300,
-        "repetition_penalty": 1.2,
-        "do_sample": True,
-        "top_k": 5,
-        "top_p": 0.95,
-    },
+ADAPTER_PATH = "/code/langchain_summ/models/Mistral_dialogsum_v2"
+tokenizer = AutoTokenizer.from_pretrained(ADAPTER_PATH)
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_use_double_quant=True,
+)
+device_map = "auto"
+model = AutoModelForCausalLM.from_pretrained(
+    ADAPTER_PATH,
+    quantization_config=bnb_config,
+    device_map=device_map,
 )
 
-template = "{question}"
-prompt = PromptTemplate(template=template, input_variables=["question"])
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=400)
+hfp = HuggingFacePipeline(pipeline=pipe)
+
+template = """Context:
+{context} 
+    
+[INST]Summarizing the following text:[/INST]
+Output:"""
+prompt = PromptTemplate(template=template, input_variables=["context"])
 output_parser = StrOutputParser()
 
-chain = prompt | rugpt | output_parser
+chain = prompt | hfp | output_parser
